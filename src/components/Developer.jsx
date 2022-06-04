88a99dba1f6c1a1f4cf240caf1591e2e1781c1f5
Adebola <adebolaakomolafe@gmail.com>
2022-06-04 10:00:00 +0100

    proto: developer grid

--- src/components/Developer.jsx
+++ src/components/Developer.jsx
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
 import "./Developer.css";
 
 export default function Developer() {
-  return <div className="developer_container"></div>;
+  return (
+    <div className="developer_container">
+      <div src="" alt="" className="developer_banner"></div>
+      <div className="developer_info">
+        <div className="developer_photo"></div>
+        <div className="developer_details">
+          <div className="developer_id">
+            <h4 className="developer_name">Name</h4>
+            <div className="developer_price">Price</div>
+          </div>
+          <Link to="#" className="developer_hire_button">
+            Hire
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
